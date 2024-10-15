51b0c423718de1b67681a717b2b263392ba186f9
valiottaGH <valentinoliotta3691@gmail.com>
2024-10-15 13:11:57 -0300

    solucionando errores

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,8 +3,10 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    placeholder?: string;  /* se agregan miembros para que funcione la inteface */
+  }
+  
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
