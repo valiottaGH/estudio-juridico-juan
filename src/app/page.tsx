de5c6af495e23d55204882a92c9d6dce46ac8436
valiottaGH <valentinoliotta3691@gmail.com>
2024-10-17 20:39:33 -0300

    cambios de diseño, link a whatsapp y api para el envio del ormulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 /** pagina para un estudio juridico */
 /** tecnologias implementadas:Next.js, tailwind, javascript, node */
+"use client"; //para poder utilizar useState
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { Car, BookOpen, Phone, Mail, MapPin } from "lucide-react"
+import { Car, BookOpen, Phone, Mail, MapPin, Gavel,} from "lucide-react"
 import Link from "next/link"
 import Image from "next/image"
 import LinkedInLogo from "@/components/iconos/linkedin"
+import React from "react";
 
+
+
+
+const urlWhatsapp = "https://wa.me/5491170011365?text=Escriba%20su%20consulta";
 export default function LandingPage() {
   return (
     /** header de la pagina */
@@ -34,29 +40,36 @@ export default function LandingPage() {
       </header>
       {/** presentacion */}
       <main className="flex-grow">
-        <section id="inicio" className="bg-gray-100 py-20">
+        <section id="inicio" className="bg-[url('/fondo.jpg')] bg-cover bg-center py-20">
           <div className="container mx-auto px-4 text-center">
-            <h2 className="text-4xl font-bold mb-4 text-burgundy-600">Expertos en Accidentes de Tránsito y Asesoría Legal</h2>
-            <p className="text-xl mb-8">Protegemos sus derechos y luchamos por la justicia que merece</p>
-            <Button className="bg-burgundy-600 hover:bg-burgundy-700 text-black font-bold py-2 px-4 rounded">
-              Consulta 
-            </Button>
+            <h2 className="text-4xl text-white font-bold mb-4 text-burgundy-600">Expertos en Accidentes de Tránsito y Asesoría Legal</h2>
+            <p className="text-xl text-white mb-8">Protegemos sus derechos y luchamos por la justicia que merece</p>
+            
+            <Link href={urlWhatsapp} target="_blank ">
+            <button className=" bg-green-500 text-white py-2 px-4 rounded shadow-lg transform transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-1 hover:bg-green-600">Enviar consulta</button>
+            </Link>
+           
           </div>
         </section>
 
-        <section id="servicios" className="py-20">
+        <section id="servicios" className="bg-blue-100 py-20">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold text-center mb-12 text-burgundy-600">Nuestros Servicios</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <div className="bg-white p-6 rounded-lg shadow-md border-t-4 border-burgundy-600">
+              <div className="bg-blue-300 p-6 rounded-lg shadow-md border-t-4 border-burgundy-600">
                 <Car className="w-12 h-12 text-burgundy-600 mb-4" />
                 <h3 className="text-xl font-semibold mb-2">Accidentes de Tránsito</h3>
                 <p>Representación legal experta para víctimas de accidentes de tránsito. Obtenga la compensación que merece.</p>
               </div>
-              <div className="bg-white p-6 rounded-lg shadow-md border-t-4 border-burgundy-600">
+              <div className="bg-blue-300  p-6 rounded-lg shadow-md border-t-4 border-burgundy-600">
                 <BookOpen className="w-12 h-12 text-burgundy-600 mb-4" />
                 <h3 className="text-xl font-semibold mb-2">Asesoría Legal</h3>
                 <p>Asesoramiento jurídico integral en diversas áreas del derecho. Soluciones personalizadas para sus necesidades legales.</p>
+              </div>
+              <div className="bg-blue-300  p-6 rounded-lg shadow-md border-t-4 border-burgundy-600">
+                <Gavel className="w-12 h-12 text-burgundy-600 mb-4" />
+                <h3 className="text-xl font-semibold mb-2">Derecho Penal</h3>
+                <p>Defensa legal sólida para aquellos acusados de delitos. Protección de derechos y búsqueda de justicia en cada caso.</p>
               </div>
             </div>
           </div>
@@ -67,14 +80,14 @@ export default function LandingPage() {
             <h2 className="text-3xl font-bold text-center mb-12 text-burgundy-600">Contáctenos</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div>
-                <form className="space-y-4">
-                  <Input type="text" placeholder="Nombre completo" required />
-                  <Input type="email" placeholder="Correo electrónico" required />
-                  <Input type="tel" placeholder="Teléfono" />
-                  <Textarea placeholder="Describa brevemente su caso" required />
-                  <Button type="submit" className="w-full bg-burgundy-600 hover:bg-burgundy-700 text-black font-bold py-2 px-4 rounded">
-                    Enviar Consulta
-                  </Button>
+                <form className="space-y-4" action="https://formsubmit.co/juanpabloliotta@live.com.ar" method="POST">
+                  <Input name="nombre" type="text" placeholder="Nombre completo" required />
+                  <Input name="email" type="email" placeholder="Correo electrónico" required />
+                  <Input name="tel" type="tel" placeholder="Teléfono" />
+                  <Textarea name="comments" placeholder="Describa brevemente su caso" required />
+                  <Input name="enviar" type="submit" value={"enviar"} className="bg-blue-500 text-white font-bold py-2 px-4 rounded transition transform hover:scale-105 hover:bg-blue-600 hover:shadow-lg"/> 
+                  <Input type="hidden" name="_next" value="http://localhost:3000"/>
+                  <Input type="hidden" name="_subject" value="Nuevo caso!"/>
                 </form>
               </div>
               <div className="space-y-4">
